e8abea835c5d10f748fe98ec3550835fbc5e9db3
Taylor Mills <taylormills24@yahoo.com>
2019-02-19 13:08:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 grandma = "I love you grandma!"
 
-if grandma == "Huh?! Speak up Sonny!".upcase
+if grandma == "Huh?! Speak up, Sonny!".upcase
  puts "grandson speaks up."
 elsif grandma == "No not since 1938!".upcase 
   puts "grandma responds to grandson." 

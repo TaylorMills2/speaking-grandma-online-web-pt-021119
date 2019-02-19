96229b009a4e582df09b1fe80796168fc4311024
Taylor Mills <taylormills24@yahoo.com>
2019-02-19 13:11:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,8 @@ grandma = "I love you grandma!"
 if grandma == "Huh?! Speak up, Sonny!".upcase
  puts "Sonny speaks up."
 elsif grandma == "No not since 1938!".upcase 
-  puts "grandma responds to grandson." 
-elsif Sonny == "I love you grandma!".upcase
+  puts "grandma responds to Sonny." 
+elsif grandma == "I love you grandma!".upcase
  puts "Sonny responds to grandma."
 else 
   puts "I love you too pumpkin!"
